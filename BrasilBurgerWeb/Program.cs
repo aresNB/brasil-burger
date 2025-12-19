f97fc468b28906b9de1d26d38c2f9ececd20bd86
ares <bienvenungwanga@gmail.com>
2025-12-19 21:54:39 +0000

    feat: Configuration des sessions pour authentification

--- BrasilBurgerWeb/Program.cs
+++ BrasilBurgerWeb/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configuration des sessions
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +34,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
