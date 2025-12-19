d0e860117228a459a6fafe1e48a7cfb4321fc2b3
ares <bienvenungwanga@gmail.com>
2025-12-19 14:09:48 +0000

    feat: Modification de CatalogueController.cs

--- BrasilBurgerWeb/Controllers/CatalogueController.cs
+++ BrasilBurgerWeb/Controllers/CatalogueController.cs
@@ -1,3 +1,100 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using BrasilBurgerWeb.Data;
+// using BrasilBurgerWeb.Models;
+
+// namespace BrasilBurgerWeb.Controllers
+// {
+//     public class CatalogueController : Controller
+//     {
+//         private readonly ApplicationDbContext _context;
+
+//         public CatalogueController(ApplicationDbContext context)
+//         {
+//             _context = context;
+//         }
+
+//         // GET: Catalogue
+//         public async Task<IActionResult> Index()
+//         {
+//             var burgers = await _context.Burgers
+//                 .Include(b => b.Categorie)
+//                 .Where(b => !b.IsArchived)
+//                 .OrderBy(b => b.Libelle)
+//                 .ToListAsync();
+
+//             return View(burgers);
+//         }
+
+//         // GET: Catalogue/Details/5
+//         public async Task<IActionResult> Details(int id)
+//         {
+//             var burger = await _context.Burgers
+//                 .Include(b => b.Categorie)
+//                 .FirstOrDefaultAsync(b => b.Id == id);
+
+//             if (burger == null)
+//                 return NotFound();
+
+//             return View(burger);
+//         }
+
+//     }
+// }
+
+
+// =================================================
+
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using BrasilBurgerWeb.Data;
+// using BrasilBurgerWeb.Models;
+
+// namespace BrasilBurgerWeb.Controllers
+// {
+//     public class CatalogueController : Controller
+//     {
+//         private readonly ApplicationDbContext _context;
+
+//         public CatalogueController(ApplicationDbContext context)
+//         {
+//             _context = context;
+//         }
+
+//         // GET: Catalogue
+//         public async Task<IActionResult> Index()
+//         {
+//             var burgers = await _context.Burgers
+//                 .Include(b => b.Categorie)
+//                 .Where(b => !b.IsArchived)
+//                 .ToListAsync();
+
+//             return View(burgers);
+//         }
+
+//         // GET: Catalogue/Details/5
+//         public async Task<IActionResult> Details(int? id)
+//         {
+//             if (id == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             var burger = await _context.Burgers
+//                 .Include(b => b.Categorie)
+//                 .FirstOrDefaultAsync(m => m.Id == id);
+
+//             if (burger == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             return View(burger);
+//         }
+//     }
+// }
+
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BrasilBurgerWeb.Data;
@@ -14,30 +111,110 @@ namespace BrasilBurgerWeb.Controllers
             _context = context;
         }
 
-        // GET: Catalogue
-        public async Task<IActionResult> Index()
+        // GET: Catalogue - Tous les produits
+        public async Task<IActionResult> Index(string? filtre)
         {
+            ViewBag.FiltreActif = filtre ?? "tous";
+
+            if (filtre == "burgers")
+            {
+                return RedirectToAction(nameof(Burgers));
+            }
+            else if (filtre == "menus")
+            {
+                return RedirectToAction(nameof(Menus));
+            }
+            else if (filtre == "complements")
+            {
+                return RedirectToAction(nameof(Complements));
+            }
+
+            // Par défaut : tous les produits
             var burgers = await _context.Burgers
                 .Include(b => b.Categorie)
                 .Where(b => !b.IsArchived)
-                .OrderBy(b => b.Libelle)
                 .ToListAsync();
 
             return View(burgers);
         }
 
-        // GET: Catalogue/Details/5
-        public async Task<IActionResult> Details(int id)
+        // GET: Catalogue/Burgers
+        public async Task<IActionResult> Burgers()
         {
+            ViewBag.FiltreActif = "burgers";
+            var burgers = await _context.Burgers
+                .Include(b => b.Categorie)
+                .Where(b => !b.IsArchived)
+                .ToListAsync();
+
+            return View("Index", burgers);
+        }
+
+        // GET: Catalogue/Menus
+        public async Task<IActionResult> Menus()
+        {
+            ViewBag.FiltreActif = "menus";
+            var menus = await _context.Menus
+                .Include(m => m.Burger)
+                .Include(m => m.Boisson)
+                .Include(m => m.Frite)
+                .Where(m => !m.IsArchived)
+                .ToListAsync();
+
+            return View("ListeMenus", menus);
+        }
+
+        // GET: Catalogue/Complements
+        public async Task<IActionResult> Complements()
+        {
+            ViewBag.FiltreActif = "complements";
+            var complements = await _context.Complements
+                .Where(c => !c.IsArchived)
+                .ToListAsync();
+
+            return View("ListeComplements", complements);
+        }
+
+        // GET: Catalogue/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
             var burger = await _context.Burgers
                 .Include(b => b.Categorie)
-                .FirstOrDefaultAsync(b => b.Id == id);
+                .FirstOrDefaultAsync(m => m.Id == id);
 
             if (burger == null)
+            {
                 return NotFound();
+            }
 
             return View(burger);
         }
 
+        // GET: Catalogue/DetailsMenu/5
+        public async Task<IActionResult> DetailsMenu(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .Include(m => m.Burger)
+                .Include(m => m.Boisson)
+                .Include(m => m.Frite)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
     }
 }
--- BrasilBurgerWeb/Data/ApplicationDbContext.cs
+++ BrasilBurgerWeb/Data/ApplicationDbContext.cs
@@ -1,3 +1,34 @@
+// using Microsoft.EntityFrameworkCore;
+// using BrasilBurgerWeb.Models;
+
+// namespace BrasilBurgerWeb.Data
+// {
+//     public class ApplicationDbContext : DbContext
+//     {
+//         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//             : base(options)
+//         {
+//         }
+
+//         // DbSets
+//         public DbSet<BurgerCategorie> BurgerCategories { get; set; }
+//         public DbSet<Burger> Burgers { get; set; }
+
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)
+//         {
+//             base.OnModelCreating(modelBuilder);
+
+//             // Configuration du mapping (noms de tables en minuscules)
+//             modelBuilder.Entity<BurgerCategorie>()
+//                 .ToTable("burger_categories");
+
+//             modelBuilder.Entity<Burger>()
+//                 .ToTable("burgers");
+//         }
+//     }
+// }
+
+
 using Microsoft.EntityFrameworkCore;
 using BrasilBurgerWeb.Models;
 
@@ -13,17 +44,44 @@ namespace BrasilBurgerWeb.Data
         // DbSets
         public DbSet<BurgerCategorie> BurgerCategories { get; set; }
         public DbSet<Burger> Burgers { get; set; }
+        public DbSet<Complement> Complements { get; set; }
+        public DbSet<Menu> Menus { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Configuration du mapping (noms de tables en minuscules)
+            // Configuration des noms de tables (en minuscules pour PostgreSQL)
             modelBuilder.Entity<BurgerCategorie>()
                 .ToTable("burger_categories");
 
             modelBuilder.Entity<Burger>()
                 .ToTable("burgers");
+
+            modelBuilder.Entity<Complement>()
+                .ToTable("complements");
+
+            modelBuilder.Entity<Menu>()
+                .ToTable("menus");
+
+            // Configuration des relations Menu
+            modelBuilder.Entity<Menu>()
+                .HasOne(m => m.Burger)
+                .WithMany()
+                .HasForeignKey(m => m.BurgerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Menu>()
+                .HasOne(m => m.Boisson)
+                .WithMany()
+                .HasForeignKey(m => m.BoissonId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Menu>()
+                .HasOne(m => m.Frite)
+                .WithMany()
+                .HasForeignKey(m => m.FriteId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
